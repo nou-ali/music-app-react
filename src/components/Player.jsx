f58f34010f88e2607ccdb2ba4d4b05600fcaccf5
nou-ali <noura4403@email.com>
2022-03-11 16:25:19 -0500

    added icons for skipping music back & forward

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlay} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
@@ -11,7 +15,9 @@ const Player = () => {
         <p>End Time</p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="play" icon={faPlay}/>
+        <FontAwesomeIcon className="skip-music-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon className="skip-music-forward" size="2x" icon={faAngleRight} />
       </div>
     </div>
   );
