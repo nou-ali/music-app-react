2195aed73099af8be786b2f62f136b91776b7a2e
nou-ali <noura4403@email.com>
2022-03-12 20:31:13 -0500

    worked on state so when song is selected there is an identifier through css

--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 
-const LibrarySong = ({song, songs, setCurrentSong, audioRef, isPlaying}) => {
+const LibrarySong = ({ setSongs, song, songs, setCurrentSong, audioRef, isPlaying, id }) => {
+  const songSelectHandler = () => {
+    setCurrentSong(song);
 
-  const songSelectHandler = async () => {
-      setCurrentSong(song);
-      
-      if(isPlaying) {
-        const playPromise = audioRef.current.play();
-        if(playPromise !== undefined) {
-          playPromise.then((audio) => {
-            audioRef.current.play();
-          })
-        }
+    //add active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
       }
+    });
+
+    setSongs(newSongs);
+
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
   };
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
-      <h3>{song.name}</h3>
-      <h4>{song.artist}</h4>
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
       </div>
     </div>
   );
